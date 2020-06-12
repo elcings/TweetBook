b23a56791a45c121a4700103b3de2113af022e33
Elcin Aliyev <elcinaliyevgs@gmail.com>
2020-06-12 17:00:57 +0400

    apikey

---
+++ TweetBook/Controllers/V1/SecretController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TweetBook.Filter;
+
+namespace TweetBook.Controllers.V1
+{
+    [ApiKeyAuth]
+    public class SecretController : ControllerBase
+    {
+
+        [HttpGet("secret")]
+        public IActionResult GetSecret()
+        {
+            return Ok("I have no secret");
+        }
+    }
+}
